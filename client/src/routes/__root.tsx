c63f5203dbb04ef1e88ee734f3b88afa558a9b19
Radostin Romanov <rad.romanov@gmail.com>
2024-05-13 16:10:27 +0300

    Add navbar component

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,18 +1,16 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import Navbar from "../components/Navbar";
 
 export const Route = createRootRoute({
-  component: () => (
+  component: () => Root(),
+});
+
+function Root() {
+  return (
     <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{" "}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
-      </div>
+      <Navbar />
       <hr />
       <Outlet />
     </>
-  ),
-});
+  );
+}
