b15839e13b956564902f83c6a85fb74698defd77
Radostin Romanov <rad.romanov@gmail.com>
2024-05-13 10:48:38 +0300

    Add error handler and not found handler

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,6 @@
 import { env } from "../lib/env";
 import router from "./api";
+import { handleError, notFound } from "./lib";
 import { ExpressConfig } from "./lib/config";
 
 const PORT = env.init().getOne("SERVER_PORT");
@@ -7,5 +8,8 @@ const PORT = env.init().getOne("SERVER_PORT");
 const app = ExpressConfig();
 
 app.use("/api/v1", router);
+
+app.use("*", notFound);
+app.use(handleError);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- backend/lib/middlewares.ts
+++ backend/lib/middlewares.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import z from "zod";
+import Exception from "./exception";
 
 export const bodyOfType =
   (schema: z.ZodSchema) =>
@@ -7,8 +8,37 @@ export const bodyOfType =
     const payload = schema.safeParse(req.body);
 
     if (!payload.success) {
-      return next(new Error("Invalid payload in request body."));
+      return next(
+        new Exception(
+          "Invalid payload in request body.",
+          "Unprocessable Entity"
+        )
+      );
     }
 
     next();
   };
+
+export const notFound = (req: Request, _res: Response, next: NextFunction) => {
+  next(new Exception(`Path ${req.originalUrl} not found.`, "Not Found"));
+};
+
+export const handleError = (
+  error: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  if (error instanceof Exception) {
+    const { exception, message, path, status } = error.serialize();
+
+    return res.status(status).send({ message, exception, status, path });
+  }
+
+  return res.status(500).send({
+    message: "Unexpected error occurred. Please, try again.",
+    exception: "Internal Server Error",
+    status: 500,
+    path: null,
+  });
+};
