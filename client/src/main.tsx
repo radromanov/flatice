b2fdb76493340120a97d3745cdc41e8ca2fe029a
Radostin Romanov <rad.romanov@gmail.com>
2024-05-13 21:48:47 +0300

    Add tanstack query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+
 import { RouterProvider, createRouter } from "@tanstack/react-router";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { routeTree } from "./routeTree.gen";
-
-const router = createRouter({ routeTree });
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
@@ -14,8 +13,13 @@ declare module "@tanstack/react-router" {
   }
 }
 
+const router = createRouter({ routeTree });
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- client/src/routes/expenses.tsx
+++ client/src/routes/expenses.tsx
@@ -1,9 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import { api } from "../../../lib";
 
 export const Route = createFileRoute("/expenses")({
   component: () => Expenses(),
 });
 
+async function getExpenses() {
+  const res = await api.expenses.getAll("admin");
+  return res;
+}
+
 function Expenses() {
-  return <div>Show all expenses here</div>;
+  const { isPending, error, data } = useQuery({
+    queryKey: ["get-all-expenses"],
+    queryFn: getExpenses,
+  });
+
+  console.log(data);
+
+  if (isPending) return "Loading...";
+  if (error) return null;
+
+  return (
+    <div>
+      <ul>
+        {data.map((expense) => (
+          <li key={expense.id}>
+            <span>{expense.type}</span>
+            <span>{expense.amount}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
