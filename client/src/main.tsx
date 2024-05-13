f7bfab8ff59f1793ed23db11192ea5f174abbb1d
Radostin Romanov <rad.romanov@gmail.com>
2024-05-13 16:03:11 +0300

    Add TanStack router and initial home and about pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,5 @@
 export * from "./constants";
+export * from "./api";
+export * from "./env";
+export * from "./helpers";
+export * from "./types";
