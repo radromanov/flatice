8cb8ea9faee030d77c6ca801f1a608b177414a0d
Radostin Romanov <rad.romanov@gmail.com>
2024-05-17 12:09:23 +0300

    Clean up middleware, remove deprecated sentry config files

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,9 +1,30 @@
-export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('./sentry.server.config');
+import * as Sentry from "@sentry/nextjs";
+
+export function register() {
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    Sentry.init({
+      dsn: "https://23e2ff17c54d43e3170a0718f93a5625@o1363078.ingest.us.sentry.io/4507266621046784",
+
+      // Adjust this value in production, or use tracesSampler for greater control
+      tracesSampleRate: 1,
+
+      // Setting this option to true will print useful information to the console while you're setting up Sentry.
+      debug: false,
+
+      // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+      // spotlight: process.env.NODE_ENV === 'development',
+    });
   }
 
-  if (process.env.NEXT_RUNTIME === 'edge') {
-    await import('./sentry.edge.config');
+  if (process.env.NEXT_RUNTIME === "edge") {
+    Sentry.init({
+      dsn: "https://23e2ff17c54d43e3170a0718f93a5625@o1363078.ingest.us.sentry.io/4507266621046784",
+
+      // Adjust this value in production, or use tracesSampler for greater control
+      tracesSampleRate: 1,
+
+      // Setting this option to true will print useful information to the console while you're setting up Sentry.
+      debug: false,
+    });
   }
 }
--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,22 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { withAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextResponse, type NextRequest } from "next/server";
 
-// const privateRoutes = ["/expenses"];
-const publicRoutes = ["/", "/api/auth"];
+const publicRoutes = ["/"];
 
 export default async function middleware(req: NextRequest) {
-  const isAuthed = await getKindeServerSession().isAuthenticated();
   const path = req.nextUrl.pathname;
   const isPublicRoute = publicRoutes.includes(path);
-  // const isPrivateRoute = privateRoutes.includes(path);
 
-  // If not authed, and attempting to access a private route, redirect to home page
-  if (!isAuthed && !isPublicRoute) {
-    return NextResponse.redirect(new URL("/", req.url));
+  // If attempting to access a non-public route, a.k.a. private route (e.g. /expenses, /profile, etc.)...
+  if (!isPublicRoute) {
+    // Redirects to login screen
+    return withAuth(req);
   }
 
-  // Attach the url to headers
-  const response = NextResponse.next();
-  response.headers.set("x-url", req.url);
-
-  return response;
+  return NextResponse.next();
 }
 
-// Routes Middleware should not run on
+// Protected routes
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
