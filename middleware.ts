7d4bacf4cb6da05c1118b29016c403da6d0dc7fa
Radostin Romanov <rad.romanov@gmail.com>
2024-05-17 10:58:27 +0300

    Add /api/auth endpoint to public routes in middleware

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 // const privateRoutes = ["/expenses"];
-const publicRoutes = ["/"];
+const publicRoutes = ["/", "/api/auth"];
 
 export default async function middleware(req: NextRequest) {
   const isAuthed = await getKindeServerSession().isAuthenticated();
